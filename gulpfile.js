171b1b7837d832c1efaed6db78b26d28fd138ba7
fabricioLJdev <fabricioj516gb@outlook.com>
2025-02-20 20:10:39 -0300

    criando o hero e o minificador de imagens

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 const gulp = require('gulp'); //importando o gulp
 const sass = require('gulp-sass')(require('sass')); //importando o compilador sass
+const imagemin = require('gulp-imagemin'); //importando o pluggin que instalamos na versão @7.1.0
+
+            // *** Lembrando que para minificar as imagens com o imagemin, é preciso instalar o gulp na versão: 4.2.0 (npm install --save-dev gulp@4.2.0) ***
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(gulp.dest('./dist/css'));
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+
+
+exports.default = gulp.parallel(styles, images); //pedindo para função principal execultar o styles e images
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
