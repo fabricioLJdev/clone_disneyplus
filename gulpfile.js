505dbcc5fbc7d99e2369e59436295b6f00d82536
fabricioLJdev <fabricioj516gb@outlook.com>
2025-03-10 20:07:46 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp'); //importando o gulp
 const sass = require('gulp-sass')(require('sass')); //importando o compilador sass
 const imagemin = require('gulp-imagemin'); //importando o pluggin que instalamos na versão @7.1.0
+const uglify = require('gulp-uglify');
 
             // *** Lembrando que para minificar as imagens com o imagemin, é preciso instalar o gulp na versão: 4.2.0 (npm install --save-dev gulp@4.2.0) ***
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -18,8 +25,9 @@ function images() {
 
 
 
-exports.default = gulp.parallel(styles, images); //pedindo para função principal execultar o styles e images
+exports.default = gulp.parallel(styles, images, scripts); //pedindo para função principal execultar o styles e images
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
